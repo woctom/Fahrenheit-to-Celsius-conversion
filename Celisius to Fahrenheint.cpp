45609a3cb4d0fbd0de03603dc9a7235c2fd89c3f
Wellington de Oliveira Cardoso <35791849+woctom@users.noreply.github.com>
2018-01-29 21:49:59 -0200

    Add files via upload

---
+++ Celisius to Fahrenheint.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	float Celsius;
+    float Fahrenheint;
+    
+    			cout<<"type the temperature in Celsius:"<<endl;
+    			
+    			cin>>Celsius;
+    			
+    	
+    	Fahrenheint=Celsius*1.8+32;
+    			
+    		cout<<Celsius<<"degrees celsius is equal to"<<Fahrenheint<<"degrees fahrenheint";
+    		
+    															return 0;
+    }
